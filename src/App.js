7328731303ce0816f247ea44ca7fb040c3284654
nitesh047 <niteshsaini5112001@gmail.com>
2023-02-28 01:37:31 +0530

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
 
 export default function App() {
   return (
-    <div>Adhira</div>
+    <Navbar/>
   )
 }
