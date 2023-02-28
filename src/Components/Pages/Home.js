365e318ec57831ab096d6ded8934da55978d536c
nitesh047 <niteshsaini5112001@gmail.com>
2023-02-28 16:12:02 +0530

    working on Home Page

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+  <div className='container-fluid' style={{background:'#faede3',height:'80vh'}}>
+    <div className='container'>
+    <div className='col-md-8'>
+      <br></br><br></br><br></br><br></br>
+<p style={{fontSize:'75px', fontWeight:'700'}}>Hire World's Best <span style={{color:'#006D77'}}>Talent</span><br></br> <span style={{color:'#006D77'}}>Within</span> 24 Hours</p>
+ 
+ <p style={{color:'rgb(102, 112, 133)' ,fontSize:'24px'}}>Miraclesoftsolutions is a flexible platform to vet and hire a talent like <br></br>
+ software developers, 
+designers and quality assurance engineers that  <br></br>
+match your time  zone and work model. 
+							
+ </p>   
+    </div>
+    <div className='col-md-4'>
+
+    </div>
+    </div>
+  </div>
   )
 }
 
